cd6d97d498def6b0e948dc20a09fa496b9f3d58d
wold21 <wold2180@gmail.com>
2021-11-06 15:50:55 +0900

    Navigator.pushReplacementNamed, data to front

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -12,14 +12,14 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  String? time = 'loading';
-
   void setupWorldTime() async {
     WorldTime instance =
         WorldTime(location: 'Seoul', flag: 'seoul.png', url: 'Asia/Seoul');
     await instance.getTime();
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time
     });
   }
 
@@ -34,7 +34,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
         body: Padding(
       padding: EdgeInsets.all(50.0),
-      child: Text(time!),
+      child: Text('loading'),
     ));
   }
 }
